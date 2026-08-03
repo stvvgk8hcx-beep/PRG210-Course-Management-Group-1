3ce4e49e0d388cd57d3aac9196e7c3815c397e58
stvvgk8hcx-beep <240383088+stvvgk8hcx-beep@users.noreply.github.com>
2026-08-03 14:59:48 -0400

    added pragma once and const to department h
    will add const to cpp as well

--- Header Files/department.h
+++ Header Files/department.h
@@ -1,50 +1,34 @@
-#ifndef DEPARTMENT_H
-#define DEPARTMENT_H
-
+#pragma once
 // COMMIT NOTES - Calvin
 // I made this file right after I did course. reasoning is Department will hold many courses is
-
 // Course = one thing, Department = many things.
-
-
 #include "course.h" //access the course objects we need for departments to access to
-
 
 class Department
 {
     private:
 
         char departmentName[50]; // we need char array for the name
-
         Course* courses; // pointer to a dynamic array of course so will grow at runtime
-
         int courseCount; // stores an integer for the amount of classes they have in the department
-
 
     public:
 
         Department(); //default constructo week 8
-
         Department(const char* name); //overload constructor
-
         ~Department(); //deconstructor free the dynamic array different than course!  week 8 p19
-
         Department(const Department& other);            // copy constructor
         Department& operator=(const Department& other); // copy-assignment rule of threeeeeee
-
         void addCourse(Course c); // will grow this array by one and stores the course
-
         void listCourse();  // will print the courses in a list by reusing Course::display
         //see course.cpp line 31
     // comment even said "Department WILL need a destructor" - past me was right! go past me
-        const char* getName();
 
-
-
-        int getCourseCount(); // display the number of courses
-        Course* getCourse(int index); // this will come
+        const char* getName() const;
+// added const to these as well
+        int getCourseCount() const; // display the number of courses
+        Course* getCourse(int index) const; // this will come
         // now returns Course as a pointer like the prof CVS
-
 };
 
-#endif
+// Now uses pragma once instead of ifndef define endif to save lots of space and such 
